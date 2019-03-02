943939567893317977a94d38827984ddf977e5a3
Dominik Zduńczyk <dominik.zdunczyk@enxoo.com>
2019-03-02 02:05:47 +0100

    Change tokens.

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,5 +1,5 @@
 # YOUR_APP/config/initializers/messenger.rb
 Messenger.configure do |config|
-  config.verify_token      = ENV['VERIFY_TOKEN'] #will be used in webhook verifiction
-  config.page_access_token = ENV['PAGE_ACCESS_TOKEN']
+  config.verify_token      = '41055c406df48dbc11b903bae55dccbc' #will be used in webhook verifiction
+  config.page_access_token = "EAAIRCVllojkBAJHtyl1ZCBuzlrTDbGw1qvVC04ugpXGsAHUIKAv8GpsawJ0oVlaZCTBm2AJ9R3iDeSRonyZAIbn3yvUwgQRFSUHIk5cUJZBbyxvIJsuSXOlmFFEbKrIjHVWtZBlwTNdoMXGurnsdozMdhRw5p5t0K10ZBeSjEVzJEstrCrHOUL"
 end
