3538b43bf75cd4d07cb7b712c97b30fa5b4b293c
Dominik Zduńczyk <dominik.zdunczyk@enxoo.com>
2019-03-03 01:02:40 +0100

    RESTET.

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,5 +1,5 @@
 # YOUR_APP/config/initializers/messenger.rb
 Messenger.configure do |config|
-  config.verify_token      = 'test_token' #will be used in webhook verifiction
-  config.page_access_token = 'EAAGZBgihQheYBAPNlB5CF4QHRGwh5Gb6jlb0nT4ddSvPTv9XWGJZBY6bLfW18SrPECKzjVXFbhUKn4FRLhjmd5jqy93j5L2pGHwCDZANTMkqzCPWLEXLzDjpFgKKtWPiZBAZBLOy3KdimFZBmgFVZBiEOxXNA52pPWufgQgb9BdPBG1RqONIFtk'
+  config.verify_token      = ENV['VERIFY_TOKEN'] #will be used in webhook verifiction
+  config.page_access_token = ENV['PAGE_ACCESS_TOKEN']
 end
